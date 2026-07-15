1f34a4c873d730029edc32bb91a6a96085931f32
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-15 23:00:17 +0530

    Post create added

--- app.js
+++ app.js
@@ -4,12 +4,41 @@ const Cors=require("cors")
 const Bcrypt=require("bcrypt")
 const jwt=require("jsonwebtoken")
 const userModel=require("./models/users")
+const postModel = require("./models/posts");
 
 let app=Express()
 app.use(Express.json())
 app.use(Cors())
 
 Mongoose.connect("mongodb://anaghaajidev:Anagha23@ac-agnsoij-shard-00-00.lk3iujt.mongodb.net:27017,ac-agnsoij-shard-00-01.lk3iujt.mongodb.net:27017,ac-agnsoij-shard-00-02.lk3iujt.mongodb.net:27017/blogapp_db?ssl=true&replicaSet=atlas-bjgzxl-shard-0&authSource=admin&appName=Cluster0")
+
+//create a post
+app.post("/create", async (req, res) => {
+
+    let input = req.body;
+    let token = req.headers.token;
+
+    jwt.verify(token, "blogApp", async (error, decoded) => {
+
+        if (error) {
+            return res.json({
+                status: "Invalid Token"
+            });
+        }
+
+        input.postedDate = new Date();
+
+        let result = new postModel(input);
+
+        await result.save();
+
+        res.json({
+            status: "Success"
+        });
+
+    });
+
+});
 
 //signIn
 app.post("/signin",async(req,res)=>{
