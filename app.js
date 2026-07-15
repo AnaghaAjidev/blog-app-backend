2247af90df897675122150976cc3d9b12ac98359
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-15 23:12:48 +0530

    Viewall created

--- app.js
+++ app.js
@@ -40,6 +40,30 @@ app.post("/create", async (req, res) => {
 
 });
 
+
+//view all post
+app.post("/viewall",(req,res)=>{
+
+    let token=req.headers.token
+    jwt.verify(token,"blogApp",(error,decoded)=>{
+
+        if (decoded && decoded.email) {
+            
+            postModel.find().then(
+                (items)=>{
+                    res.json(items)
+                }
+            ).catch(
+                (error)=>{
+                    res.json({"status":"error"})
+                }
+            )
+        } else {
+             res.json({"status":"Invalid Authentication"})
+        }
+    })
+})
+
 //signIn
 app.post("/signin",async(req,res)=>{
 
