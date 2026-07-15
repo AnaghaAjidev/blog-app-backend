7f915adcc9a7a66caf33b33c22782b4c0ed5ca1b
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-15 23:29:19 +0530

    View my post added

--- app.js
+++ app.js
@@ -64,6 +64,31 @@ app.post("/viewall",(req,res)=>{
     })
 })
 
+
+//view my post
+app.post("/viewmypost",(req,res)=>{
+
+    let input=req.body
+    let token=req.headers.token
+    jwt.verify(token,"blogApp",(error,decoded)=>{
+
+        if (decoded && decoded.email) {
+            
+            postModel.find(input).then(
+                (items)=>{
+                    res.json(items)
+                }
+            ).catch(
+                (error)=>{
+                    res.json({"status":error})
+                }
+            )
+        } else {
+             res.json({"status":"Invalid Authentication"})
+        }
+    })
+})
+
 //signIn
 app.post("/signin",async(req,res)=>{
 
